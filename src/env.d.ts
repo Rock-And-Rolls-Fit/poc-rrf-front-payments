52e6238bc840d75f9a4c6dbd8eac7f6d1e6ba0f5
Joise Garcia <alejandrogar17@gmail.com>
2025-07-30 01:56:35 -0400

    chore: update package dependencies, add new payment pages, and implement accordion component for better UI

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,6 @@
 interface ImportMetaEnv {
   readonly PUBLIC_API_URL: string;
   readonly PUBLIC_API_LOCAL: string;
-  readonly API_ARTICLES: string;
-  readonly RAPID_API_KEY: string;
-  readonly RAPID_API_HOST: string;
   readonly PUBLIC_CLERK_PUBLISHABLE_KEY: string;
   readonly CLERK_SECRET_KEY: string;
 }
